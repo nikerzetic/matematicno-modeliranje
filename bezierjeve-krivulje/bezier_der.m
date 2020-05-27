7b2b684d18de76f20806ddf1d1a28a6a3be033f1
Nik Erzetic <nikerzetic@gmail.com>
2020-05-27 14:11:23 +0200

    Popravek vaj 13

--- bezierjeve-krivulje/bezier_der.m
+++ bezierjeve-krivulje/bezier_der.m
@@ -2,16 +2,7 @@ function v = bezier_der(b,t)
 % Funkcija bezier_der s pomo;jo de Casteljaujevaga algoritma izracuna
 % tangentni vektor dane Bezierjeve krivulje pri paramatru t.
 
-n = length(b)-1;
-B = @(j) nchoosek(n-1,j).*t.^j.*(1-t).^(n-j-1);
-v = [0;0];
-
-for i = 1:n
-   
-    v =  v + (b(:,i+1) - b(:,i)).*B(i-1);
-    
-end
-
-v = v * n;
+b = (length(b)-1) * diff(b,1,2);
+v = deCasteljau(b,t);
 
 end
